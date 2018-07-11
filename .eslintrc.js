43185ae4cad7e4e9667ddce78252e114a49e9cce
bandesz <bandesz@gmail.com>
2018-07-11 16:14:22 +0100

    eslint: set indentation for switch

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'prettier/flowtype',
   ],
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
