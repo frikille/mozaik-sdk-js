cdb5610de7c895b919c40bd4957687eccfc78862
bandesz <bandesz@gmail.com>
2018-07-13 17:06:10 +0100

    Add apply function to create all content types from the schema using the API

--- src/fields/index.js
+++ src/fields/index.js
@@ -1,4 +1,8 @@
-const createField = require('./create');
+// @flow
+const createField = require('./create/index.js');
+import type { FieldInput } from './create/index.js';
+
+export type Field = { id: string } & FieldInput;
 
 const field = {
   create: createField,
