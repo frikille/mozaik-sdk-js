c4f3f5bbbb75ed0e62cfe1a7d835e8aa6dc874ab
bandesz <bandesz@gmail.com>
2018-07-22 12:24:37 +0100

    Add min/max validation for Int/Float fields, run validations by field type

--- src/schema/create-field-validation-inputs.js
+++ src/schema/create-field-validation-inputs.js
@@ -1,8 +1,10 @@
 // @flow
 const FieldValidationInput = require('../fields/validations/create/index.js');
+import type { GraphQLInputType } from 'graphql';
 import type { FieldDefinitionNode, DirectiveNode } from 'graphql/language/ast';
 const getArgumentValue = require('./get-argument-value.js');
-const { GraphQLInt, GraphQLString } = require('graphql');
+const getFieldType = require('./get-field-type.js');
+const { GraphQLInt, GraphQLFloat, GraphQLString } = require('graphql');
 
 function createLengthValidation(
   field: FieldDefinitionNode,
@@ -28,20 +30,65 @@ function createLengthValidation(
     return [];
   }
 
-  if (typeof min !== 'undefined') {
+  if (typeof min !== 'undefined' && typeof max !== 'undefined') {
+    inputs.push({
+      type: 'LENGTH_RANGE',
+      config: { lengthMin: parseInt(min), lengthMax: parseInt(max) },
+      errorMessage: errorMessage,
+    });
+  } else if (typeof min !== 'undefined') {
     inputs.push({
       type: 'MIN_LENGTH',
       config: { lengthMin: parseInt(min) },
       errorMessage: errorMessage,
     });
-  }
-
-  if (typeof max !== 'undefined') {
+  } else {
     inputs.push({
       type: 'MAX_LENGTH',
       config: { lengthMax: parseInt(max) },
       errorMessage: errorMessage,
     });
+  }
+
+  return inputs;
+}
+
+function createMinMaxValidation(
+  field: FieldDefinitionNode,
+  directive: DirectiveNode,
+  type: GraphQLInputType
+): Array<FieldValidationInput> {
+  const inputs: Array<FieldValidationInput> = [];
+
+  const { arguments: args = [] } = directive;
+  const min = getArgumentValue(args, 'min', type, () => {});
+  const max = getArgumentValue(args, 'max', type, v => {
+    if (typeof min !== 'undefined' && Number(v) < Number(min)) {
+      throw new Error('max should be equal or greater than min');
+    }
+  });
+  const errorMessage = getArgumentValue(
+    args,
+    'errorMessage',
+    GraphQLString,
+    () => {}
+  );
+
+  if (typeof min === 'undefined' && typeof max === 'undefined') {
+    return [];
+  }
+
+  const config = {};
+  if (typeof min !== 'undefined' && typeof max !== 'undefined') {
+    config[`valueMin${type.toString()}`] = Number(min);
+    config[`valueMax${type.toString()}`] = Number(max);
+    inputs.push({ type: 'RANGE', config, errorMessage });
+  } else if (typeof min !== 'undefined') {
+    config[`valueMin${type.toString()}`] = Number(min);
+    inputs.push({ type: 'MIN_VALUE', config, errorMessage });
+  } else {
+    config[`valueMax${type.toString()}`] = Number(max);
+    inputs.push({ type: 'MAX_VALUE', config, errorMessage });
   }
 
   return inputs;
@@ -57,7 +104,33 @@ module.exports = function createFieldValidationInputs(
     directives.filter(d => d.name.value === 'validation') || [];
 
   for (let directive of validationDirectives) {
-    inputs.push(...createLengthValidation(field, directive));
+    const { type, hasMultipleValues } = getFieldType(field.type);
+    if (!hasMultipleValues) {
+      switch (type) {
+        case 'String':
+        case 'ID':
+        case 'SinglelineText':
+        case 'MultilineText':
+        case 'RichText':
+          inputs.push(...createLengthValidation(field, directive));
+          break;
+        case 'Int':
+          inputs.push(...createMinMaxValidation(field, directive, GraphQLInt));
+          break;
+        case 'Float':
+          inputs.push(
+            ...createMinMaxValidation(field, directive, GraphQLFloat)
+          );
+          break;
+        case 'Date':
+        case 'DateTime':
+        case 'File':
+        case 'Audio':
+        case 'Image':
+        case 'Video':
+          break;
+      }
+    }
   }
 
   return inputs;
--- src/schema/create-field-validation-inputs.test.js
+++ src/schema/create-field-validation-inputs.test.js
@@ -1,12 +1,190 @@
 const createFieldValidationInputs = require('./create-field-validation-inputs.js');
 const parse = require('./parse.js');
 
+const lengthValidation = fieldType => {
+  it('parses the min length validation correctly', () => {
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${fieldType} @validation(minLength: 5, errorMessage: "test err")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+
+    const expected = [
+      {
+        type: 'MIN_LENGTH',
+        config: { lengthMin: 5 },
+        errorMessage: 'test err',
+      },
+    ];
+
+    expect(createFieldValidationInputs(field)).toEqual(expected);
+  });
+
+  it('parses the max length validation correctly', () => {
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${fieldType} @validation(maxLength: 5, errorMessage: "test err")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+
+    const expected = [
+      {
+        type: 'MAX_LENGTH',
+        config: { lengthMax: 5 },
+        errorMessage: 'test err',
+      },
+    ];
+
+    expect(createFieldValidationInputs(field)).toEqual(expected);
+  });
+
+  it('parses the min/max length validation correctly', () => {
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${fieldType} @validation(minLength: 5, maxLength: 10, errorMessage: "test err")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+
+    const expected = [
+      {
+        type: 'LENGTH_RANGE',
+        config: { lengthMin: 5, lengthMax: 10 },
+        errorMessage: 'test err',
+      },
+    ];
+
+    expect(createFieldValidationInputs(field)).toEqual(expected);
+  });
+
+  it('allows the same min/max length values', () => {
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${fieldType} @validation(minLength: 5, maxLength: 5, errorMessage: "test err")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+
+    const expected = [
+      {
+        type: 'LENGTH_RANGE',
+        config: { lengthMin: 5, lengthMax: 5 },
+        errorMessage: 'test err',
+      },
+    ];
+
+    expect(createFieldValidationInputs(field)).toEqual(expected);
+  });
+
+  it('parses the min/max length validations separately', () => {
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${fieldType} @validation(minLength: 5, errorMessage: "test err") @validation(maxLength: 10, errorMessage: "test err 2")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+
+    const expected = [
+      {
+        type: 'MIN_LENGTH',
+        config: { lengthMin: 5 },
+        errorMessage: 'test err',
+      },
+      {
+        type: 'MAX_LENGTH',
+        config: { lengthMax: 10 },
+        errorMessage: 'test err 2',
+      },
+    ];
+
+    expect(createFieldValidationInputs(field)).toEqual(expected);
+  });
+
+  it('returns an empty array if min/max is not set', () => {
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${fieldType} @validation(errorMessage: "test err")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+
+    expect(createFieldValidationInputs(field)).toEqual([]);
+  });
+
+  it('throws an error if min is greater than max', () => {
+    const paddedFieldType = fieldType.padStart(18);
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${paddedFieldType} @validation(minLength: 5, maxLength: 4, errorMessage: "test err")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+    expect(() => createFieldValidationInputs(field)).toThrow({
+      message: 'maxLength should be equal or greater than minLength',
+      locations: [{ line: 3, column: 72 }],
+    });
+  });
+
+  it('throws an error if min length is the wrong type', () => {
+    const paddedFieldType = fieldType.padStart(18);
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${paddedFieldType} @validation(minLength: 5.1, errorMessage: "test err")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+    expect(() => createFieldValidationInputs(field)).toThrow({
+      message: 'was expecting Int',
+      locations: [{ line: 3, column: 58 }],
+    });
+  });
+
+  it('throws an error if max length is the wrong type', () => {
+    const paddedFieldType = fieldType.padStart(18);
+    const schema = `
+      type Object implements SimpleContentType {
+        field: ${paddedFieldType} @validation(maxLength: 5.1, errorMessage: "test err")
+      }
+    `;
+    const { simpleContentTypes } = parse(schema);
+    const field = simpleContentTypes[0].fields[0];
+    expect(() => createFieldValidationInputs(field)).toThrow({
+      message: 'was expecting Int',
+      locations: [{ line: 3, column: 58 }],
+    });
+  });
+};
+
 describe('createFieldValidationInputs function', () => {
-  describe('length validation', () => {
-    it('parses the min length validation correctly', () => {
+  describe('length validation on ID field', () => lengthValidation('ID'));
+
+  describe('length validation on String field', () =>
+    lengthValidation('String'));
+
+  describe('length validation on SinglelineText field', () =>
+    lengthValidation('SinglelineText'));
+
+  describe('length validation on MultilineText field', () =>
+    lengthValidation('MultilineText'));
+
+  describe('length validation on RichText field', () =>
+    lengthValidation('RichText'));
+
+  describe('min/max validation on Int field', () => {
+    it('parses the min value validation correctly', () => {
       const schema = `
         type Object implements SimpleContentType {
-          field: SinglelineText @validation(minLength: 5, errorMessage: "test err")
+          field: Int @validation(min: 5, errorMessage: "test err")
         }
       `;
       const { simpleContentTypes } = parse(schema);
@@ -14,8 +192,8 @@ describe('createFieldValidationInputs function', () => {
 
       const expected = [
         {
-          type: 'MIN_LENGTH',
-          config: { lengthMin: 5 },
+          type: 'MIN_VALUE',
+          config: { valueMinInt: 5 },
           errorMessage: 'test err',
         },
       ];
@@ -23,10 +201,10 @@ describe('createFieldValidationInputs function', () => {
       expect(createFieldValidationInputs(field)).toEqual(expected);
     });
 
-    it('parses the max length validation correctly', () => {
+    it('parses the max value validation correctly', () => {
       const schema = `
         type Object implements SimpleContentType {
-          field: SinglelineText @validation(maxLength: 5, errorMessage: "test err")
+          field: Int @validation(max: 5, errorMessage: "test err")
         }
       `;
       const { simpleContentTypes } = parse(schema);
@@ -34,8 +212,8 @@ describe('createFieldValidationInputs function', () => {
 
       const expected = [
         {
-          type: 'MAX_LENGTH',
-          config: { lengthMax: 5 },
+          type: 'MAX_VALUE',
+          config: { valueMaxInt: 5 },
           errorMessage: 'test err',
         },
       ];
@@ -43,10 +221,10 @@ describe('createFieldValidationInputs function', () => {
       expect(createFieldValidationInputs(field)).toEqual(expected);
     });
 
-    it('parses the min/max length validation correctly', () => {
+    it('parses the min/max value validation correctly', () => {
       const schema = `
         type Object implements SimpleContentType {
-          field: SinglelineText @validation(minLength: 5, maxLength: 10, errorMessage: "test err")
+          field: Int @validation(min: 5, max: 10, errorMessage: "test err")
         }
       `;
       const { simpleContentTypes } = parse(schema);
@@ -54,13 +232,8 @@ describe('createFieldValidationInputs function', () => {
 
       const expected = [
         {
-          type: 'MIN_LENGTH',
-          config: { lengthMin: 5 },
-          errorMessage: 'test err',
-        },
-        {
-          type: 'MAX_LENGTH',
-          config: { lengthMax: 10 },
+          type: 'RANGE',
+          config: { valueMinInt: 5, valueMaxInt: 10 },
           errorMessage: 'test err',
         },
       ];
@@ -68,10 +241,10 @@ describe('createFieldValidationInputs function', () => {
       expect(createFieldValidationInputs(field)).toEqual(expected);
     });
 
-    it('parses the min/max length validations separately', () => {
+    it('allows the same min/max value', () => {
       const schema = `
         type Object implements SimpleContentType {
-          field: SinglelineText @validation(minLength: 5, errorMessage: "test err") @validation(maxLength: 10, errorMessage: "test err 2")
+          field: Int @validation(min: 5, max: 5, errorMessage: "test err")
         }
       `;
       const { simpleContentTypes } = parse(schema);
@@ -79,13 +252,33 @@ describe('createFieldValidationInputs function', () => {
 
       const expected = [
         {
-          type: 'MIN_LENGTH',
-          config: { lengthMin: 5 },
+          type: 'RANGE',
+          config: { valueMinInt: 5, valueMaxInt: 5 },
+          errorMessage: 'test err',
+        },
+      ];
+
+      expect(createFieldValidationInputs(field)).toEqual(expected);
+    });
+
+    it('parses the min/max value validations separately', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Int @validation(min: 5, errorMessage: "test err") @validation(max: 10, errorMessage: "test err 2")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+
+      const expected = [
+        {
+          type: 'MIN_VALUE',
+          config: { valueMinInt: 5 },
           errorMessage: 'test err',
         },
         {
-          type: 'MAX_LENGTH',
-          config: { lengthMax: 10 },
+          type: 'MAX_VALUE',
+          config: { valueMaxInt: 10 },
           errorMessage: 'test err 2',
         },
       ];
@@ -96,7 +289,7 @@ describe('createFieldValidationInputs function', () => {
     it('returns an empty array if min/max is not set', () => {
       const schema = `
         type Object implements SimpleContentType {
-          field: SinglelineText @validation(errorMessage: "test err")
+          field: Int @validation(errorMessage: "test err")
         }
       `;
       const { simpleContentTypes } = parse(schema);
@@ -108,14 +301,203 @@ describe('createFieldValidationInputs function', () => {
     it('throws an error if min is greater than max', () => {
       const schema = `
         type Object implements SimpleContentType {
-          field: SinglelineText @validation(minLength: 5, maxLength: 4, errorMessage: "test err")
+          field: Int @validation(min: 5, max: 4, errorMessage: "test err")
         }
       `;
       const { simpleContentTypes } = parse(schema);
       const field = simpleContentTypes[0].fields[0];
       expect(() => createFieldValidationInputs(field)).toThrow({
-        message: 'maxLength should be equal or greater than minLength',
-        locations: [{ line: 3, column: 70 }],
+        message: 'max should be equal or greater than min',
+        locations: [{ line: 3, column: 47 }],
+      });
+    });
+
+    it('throws an error if min is the wrong type', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Int @validation(min: 5.1, errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+      expect(() => createFieldValidationInputs(field)).toThrow({
+        message: 'was expecting Int',
+        locations: [{ line: 3, column: 39 }],
+      });
+    });
+
+    it('throws an error if max is the wrong type', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Int @validation(max: 5.1, errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+      expect(() => createFieldValidationInputs(field)).toThrow({
+        message: 'was expecting Int',
+        locations: [{ line: 3, column: 39 }],
+      });
+    });
+  });
+
+  describe('min/max validation on Float field', () => {
+    it('parses the min value validation correctly', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(min: 5.1, errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+
+      const expected = [
+        {
+          type: 'MIN_VALUE',
+          config: { valueMinFloat: 5.1 },
+          errorMessage: 'test err',
+        },
+      ];
+
+      expect(createFieldValidationInputs(field)).toEqual(expected);
+    });
+
+    it('parses the max value validation correctly', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(max: 5.1, errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+
+      const expected = [
+        {
+          type: 'MAX_VALUE',
+          config: { valueMaxFloat: 5.1 },
+          errorMessage: 'test err',
+        },
+      ];
+
+      expect(createFieldValidationInputs(field)).toEqual(expected);
+    });
+
+    it('parses the min/max value validation correctly', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(min: 5.1, max: 10.1, errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+
+      const expected = [
+        {
+          type: 'RANGE',
+          config: { valueMinFloat: 5.1, valueMaxFloat: 10.1 },
+          errorMessage: 'test err',
+        },
+      ];
+
+      expect(createFieldValidationInputs(field)).toEqual(expected);
+    });
+
+    it('allows the same min/max value', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(min: 5.1, max: 5.1, errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+
+      const expected = [
+        {
+          type: 'RANGE',
+          config: { valueMinFloat: 5.1, valueMaxFloat: 5.1 },
+          errorMessage: 'test err',
+        },
+      ];
+
+      expect(createFieldValidationInputs(field)).toEqual(expected);
+    });
+
+    it('parses the min/max value validations separately', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(min: 5.1, errorMessage: "test err") @validation(max: 10.1, errorMessage: "test err 2")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+
+      const expected = [
+        {
+          type: 'MIN_VALUE',
+          config: { valueMinFloat: 5.1 },
+          errorMessage: 'test err',
+        },
+        {
+          type: 'MAX_VALUE',
+          config: { valueMaxFloat: 10.1 },
+          errorMessage: 'test err 2',
+        },
+      ];
+
+      expect(createFieldValidationInputs(field)).toEqual(expected);
+    });
+
+    it('returns an empty array if min/max is not set', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+
+      expect(createFieldValidationInputs(field)).toEqual([]);
+    });
+
+    it('throws an error if min is greater than max', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(min: 5.1, max: 4.1, errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+      expect(() => createFieldValidationInputs(field)).toThrow({
+        message: 'max should be equal or greater than min',
+        locations: [{ line: 3, column: 51 }],
+      });
+    });
+
+    it('throws an error if min is the wrong type', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(min: "wrong", errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+      expect(() => createFieldValidationInputs(field)).toThrow({
+        message: 'was expecting Float',
+        locations: [{ line: 3, column: 41 }],
+      });
+    });
+
+    it('throws an error if max is the wrong type', () => {
+      const schema = `
+        type Object implements SimpleContentType {
+          field: Float @validation(max: "wrong", errorMessage: "test err")
+        }
+      `;
+      const { simpleContentTypes } = parse(schema);
+      const field = simpleContentTypes[0].fields[0];
+      expect(() => createFieldValidationInputs(field)).toThrow({
+        message: 'was expecting Float',
+        locations: [{ line: 3, column: 41 }],
       });
     });
   });
