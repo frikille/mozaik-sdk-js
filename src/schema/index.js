24a30c86923a08b969327b7f3fdc43935234cf6d
bandesz <bandesz@gmail.com>
2018-09-05 16:18:11 +0100

    Export schema functions in index.js

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,7 +1,15 @@
-const createSchema = require('./create-schema');
+const exportSchema = require('./export-schema.js');
+const getSchemaDiff = require('./get-schema-diff.js');
+const importSchema = require('./import-schema.js');
+const printSchemaDiff = require('./print-schema-diff.js');
+const updateSchema = require('./update-schema.js');
 
 const schema = {
-  createSchema,
+  exportSchema,
+  getSchemaDiff,
+  importSchema,
+  printSchemaDiff,
+  updateSchema,
 };
 
 module.exports = schema;
